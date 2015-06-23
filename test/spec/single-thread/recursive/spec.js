45426000a7e76dbfbe7e3103fa97ba4a1779c964
Akihiro TAKASE <AkihiroTAKASE@users.noreply.github.com>
2015-06-24 03:13:12 +0900

    Use some query parameters to change test configuraton

--- test/spec/single-thread/recursive/spec.js
+++ test/spec/single-thread/recursive/spec.js
@@ -1,27 +1,36 @@
 describe("RecursiveQuickSort", function () {
     it("should sort an array in ascending order", function () {
-        var n = 10;
-        var ary = new Array(n);
+        var conf = {n: 10, randMax: 100, verbose: false};
+        var queryStrings = location.search.substring(1).split("&");
+        for (var i = 0, len = queryStrings.length; i < len; i++) {
+            if (queryStrings[i] !== "") {
+                var query = queryStrings[i].split("=");
+                if (query[0] === "n") conf.n = parseInt(query[1]);
+                if (query[0] === "randmax") conf.randMax = parseInt(query[1]);
+                if (query[0] === "verbose") conf.verbose = query[1];
+            }
+        }
+        var ary = new Array(conf.n);
 
         // generate the values
-        for (var i = 0; i < n; i++) {
-            ary[i] = Math.floor(Math.random() * 100);
+        for (var i = 0, count = conf.n; i < count; i++) {
+            ary[i] = Math.floor(Math.random() * conf.randMax);
         }
 
         // ------------------------------
         //  start sorting
         // ------------------------------
-        console.log("n: " + n);
-        console.log("original array: " + ary);
-        console.log("len: " + ary.length);
+        console.log("n: " + conf.n);
+        if (conf.verbose) console.log("original array: " + ary);
+        if (conf.verbose) console.log("len: " + ary.length);
         console.time("Time");
 
         RecursiveQuickSort.sort(ary, 0, ary.length - 1);
 
         console.log("------------------------------");
         console.timeEnd("Time");
-        console.log("sorted array: " + ary);
-        console.log("len: " + ary.length);
+        if (conf.verbose) console.log("sorted array: " + ary);
+        if (conf.verbose) console.log("len: " + ary.length);
 
         // ------------------------------
         //  check the result
