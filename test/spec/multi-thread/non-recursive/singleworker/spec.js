f3cefe786b84716f593987bc729db2583fb22466
Akihiro TAKASE <AkihiroTAKASE@users.noreply.github.com>
2015-06-25 15:33:08 +0900

    Add exec() function
    Ahead of evaluating the result in it() function, do asynchronous
    operation in exec() function.

--- test/spec/multi-thread/non-recursive/singleworker/spec.js
+++ test/spec/multi-thread/non-recursive/singleworker/spec.js
@@ -1,16 +1,17 @@
 describe("NonRecursiveQuickSort", function () {
-    it("should sort an array in ascending order", function () {
-        var conf = {n: 10, randMax: 100, verbose: false};
-        var queryStrings = location.search.substring(1).split("&");
-        for (var i = 0, len = queryStrings.length; i < len; i++) {
-            if (queryStrings[i] !== "") {
-                var query = queryStrings[i].split("=");
-                if (query[0] === "n") conf.n = parseInt(query[1]);
-                if (query[0] === "randmax") conf.randMax = parseInt(query[1]);
-            }
+    var conf = {n: 10, randMax: 100, verbose: false};
+    var queryStrings = location.search.substring(1).split("&");
+    for (var i = 0, len = queryStrings.length; i < len; i++) {
+        if (queryStrings[i] !== "") {
+            var query = queryStrings[i].split("=");
+            if (query[0] === "n") conf.n = parseInt(query[1]);
+            if (query[0] === "randmax") conf.randMax = parseInt(query[1]);
         }
-        var ary = new Array(conf.n);
-        var result = new Array(ary.length);
+    }
+    var ary = new Array(conf.n);
+    var result = new Array(ary.length);
+
+    function exec(done) {
         var w = new Worker(window.location.origin + "/quicksort/src/worker/non-recursive/worker.js");
 
         // message handler
@@ -30,6 +31,8 @@ describe("NonRecursiveQuickSort", function () {
                 if (conf.verbose) console.log("sorted array: " + result);
                 if (conf.verbose) console.log("len: " + result.length);
             }
+
+            done();
         };
 
         // generate the values
@@ -50,10 +53,13 @@ describe("NonRecursiveQuickSort", function () {
         /* CONSOLE */ { console.time("Time"); }
 
         w.postMessage(ary);
+    }
 
-        // ------------------------------
-        //  check the result
-        // ------------------------------
+    beforeEach(function (done) {
+        exec(done);
+    });
+
+    it("should sort an array in ascending order", function () {
         var isAscendingOrder = true;
         if (result.length > 0) {
             for (var i = 0, len = result.length; i + 1 < len; i++) {
